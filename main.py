fe3ba2cc487c2598471dbb996008eb35a94481fe
drakosam <drakosam@gmail.com>
2022-06-07 16:05:32 +0200

    start

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import sys, pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+screen = pygame.display.set_mode(size)
 
+rect = pygame.Rect(20,20,100,100)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    screen.fill(black)
+    pygame.display.flip()
